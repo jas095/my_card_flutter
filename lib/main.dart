c6ccf4e54551fcba113543475c1776a3f051f0a6
Jose Angel <jas@deb>
2020-08-08 12:40:16 -0400

    End of personal business card app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,101 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.blueGrey[50],
+                  backgroundImage: AssetImage('images/avatar_jas.jpg'),
+                ),
+                Text(
+                  'Jose Angel Silva',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'DancingScript',
+                  ),
+                ),
+                Text(
+                  'FULL-STACK DEVELOPER',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'IBMPlexSans',
+                    letterSpacing: 2.5,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 25.0,
+                    vertical: 10.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+58 424-5943844',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                        fontFamily: 'IBMPlexSans',
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 25.0,
+                    vertical: 10.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.alternate_email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'joseangelsilva29@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                        fontFamily: 'IBMPlexSans',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
